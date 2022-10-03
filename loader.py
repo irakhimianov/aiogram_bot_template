55615f56992f249486ee26e2591e54e3a2fa0b55
rkjar <rkjar@yandex.ru>
2022-10-03 12:48:11 +0500

    FSM support added

--- loader.py
+++ loader.py
@@ -1,7 +1,9 @@
 import logging
 from aiogram import Bot, Dispatcher, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from data import config
 
 logging.basicConfig(level=logging.INFO)
+storage = MemoryStorage()
 bot = Bot(token=config.TOKEN, parse_mode=types.ParseMode.HTML)
-dp = Dispatcher(bot)
+dp = Dispatcher(bot=bot, storage=storage)
